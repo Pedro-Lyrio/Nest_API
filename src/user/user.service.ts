fae6c482ffaa0420ceb945265977789bde9c10fc
Pedro Lyrio <117763768+Pedro-Lyrio@users.noreply.github.com>
2024-06-17 21:29:51 -0300

    feat: Criação de DTO's

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -30,8 +30,14 @@ export class UserService {
             where:{
                 id:userId,
             },
-            relations:['addresses']
-        })
+            relations:{
+                addresses:{
+                    city:{
+                        state:true,
+                    },
+                },
+            },
+        });
     }
 
     async getAllUser(): Promise<UserEntity[]>{
