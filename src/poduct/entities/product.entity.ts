45adc7b8776447ca53d65b38c251b146f3d128e0
Pedro Lyrio <117763768+Pedro-Lyrio@users.noreply.github.com>
2024-06-22 20:19:58 -0300

    feat:criando categoryService

--- src/poduct/entities/product.entity.ts
+++ src/poduct/entities/product.entity.ts
@@ -1,4 +1,4 @@
-import { CategoryEntity } from 'src/category/entities/category.entity';
+import { CategoryEntity } from '../../category/entities/category.entity';
 import {
   Column,
   CreateDateColumn,
@@ -8,30 +8,22 @@ import {
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
-
 @Entity({ name: 'product' })
 export class ProductEntity {
   @PrimaryGeneratedColumn('rowid')
   id: number;
-
   @Column({ name: 'name', nullable: false })
   name: string;
-
   @Column({ name: 'category_id', nullable: false })
   categoryId: number;
-
   @Column({ name: 'price', nullable: false })
   price: number;
-
   @Column({ name: 'image', nullable: false })
   image: string;
-
   @CreateDateColumn({ name: 'created_at' })
   createdAt: Date;
-
   @UpdateDateColumn({ name: 'updated_at' })
   updatedAt: Date;
-
   @ManyToOne(
     () => CategoryEntity,
     (category: CategoryEntity) => category.products,
