ed4ab1afd5ef23ae9b9d442f052f35146ca357cb
Pedro Lyrio <117763768+Pedro-Lyrio@users.noreply.github.com>
2024-06-21 23:30:28 -0300

    feat: criando getAddress usando TDD

--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -1,18 +1,18 @@
-import { Body, Controller, Param, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { AddressService } from './address.service';
 import { CreateAddressDto } from './dtos/createAddress.dto';
 import { AddressEntity } from './entities/address.entity';
 import { Roles } from '../decorators/roles.decorator';
 import { UserType } from '../user/enum/user-type.enum';
 import { UserId } from '../decorators/user-id.decorator';
+import { ReturnAddressDto } from './dtos/returnAddress.dto';
 
-
+@Roles(UserType.User, UserType.Admin)
 @Controller('address')
 export class AddressController {
 
     constructor( private readonly adressService: AddressService){}
-
-    @Roles(UserType.User)
+    
     @Post()
     @UsePipes(ValidationPipe)
     async createAddress(
@@ -20,5 +20,14 @@ export class AddressController {
         @UserId() userId: number,
     ): Promise<AddressEntity>{
         return this.adressService.createAddress(createAddressDto, userId);
+    }
+
+    @Get()
+    async findAddressByUserId(
+      @UserId() userId: number,
+    ): Promise<ReturnAddressDto[]> {
+      return (await this.adressService.findAddressByUserId(userId)).map(
+        (address) => new ReturnAddressDto(address),
+      );
     }
 }
