fb7e08981406ccc5736eb746cca0d22b2428e4c0
Pedro Lyrio <117763768+Pedro-Lyrio@users.noreply.github.com>
2024-06-23 13:50:44 -0300

    feat:Correção update product

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,47 +1,56 @@
 import {
-    Body,
-    Controller,
-    Delete,
-    Get,
-    Param,
-    Post,
-    UsePipes,
-    ValidationPipe,
-  } from '@nestjs/common';
-  import { Roles } from '../decorators/roles.decorator';
-  import { UserType } from '../user/enum/user-type.enum';
-  import { ProductEntity } from './entities/product.entity';
-  import { ProductService } from './product.service';
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { DeleteResult } from 'typeorm';
+import { Roles } from '../decorators/roles.decorator';
+import { UserType } from '../user/enum/user-type.enum';
 import { CreateProductDTO } from './dtos/create-product.dto';
 import { ReturnProduct } from './dtos/return-product.dto';
-import { DeleteResult } from 'typeorm';
-  
-  @Roles(UserType.Admin, UserType.User)
-  @Controller('product')
-  export class ProductController {
-    constructor(private readonly productService: ProductService) {}
-    @Get()
-    async findAll(): Promise<ReturnProduct[]> {
-      return (await this.productService.findAll()).map(
-        (product) => new ReturnProduct(product),
-      );
-    }
-  
-    @Roles(UserType.Admin)
-    @UsePipes(ValidationPipe)
-    @Post()
-    async createProduct(
-      @Body() createProduct: CreateProductDTO,
-    ): Promise<ProductEntity> {
-      return this.productService.createProduct(createProduct);
-      
-    }
+import { ProductEntity } from './entities/product.entity';
+import { ProductService } from './product.service';
+import { UpdateProductDTO } from './dtos/update-product.dto';
 
-    @Roles(UserType.Admin)
+@Roles(UserType.Admin, UserType.User)
+@Controller('product')
+export class ProductController {
+  constructor(private readonly productService: ProductService) {}
+  @Get()
+  async findAll(): Promise<ReturnProduct[]> {
+    return (await this.productService.findAll()).map(
+      (product) => new ReturnProduct(product),
+    );
+  }
+  @Roles(UserType.Admin)
+  @UsePipes(ValidationPipe)
+  @Post()
+  async createProduct(
+    @Body() createProduct: CreateProductDTO,
+  ): Promise<ProductEntity> {
+    return this.productService.createProduct(createProduct);
+  }
+  @Roles(UserType.Admin)
   @Delete('/:productId')
   async deleteProduct(
     @Param('productId') productId: number,
   ): Promise<DeleteResult> {
     return this.productService.deleteProduct(productId);
   }
+
+  @Roles(UserType.Admin)
+  @UsePipes(ValidationPipe)
+  @Put('/:productId')
+  async updateProduct(
+    @Body() updateProduct: UpdateProductDTO,
+    @Param('productId') productId: number,
+  ): Promise<ProductEntity> {
+    return this.productService.updateProduct(updateProduct, productId);
   }
+}
