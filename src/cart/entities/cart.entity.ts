9102d66232427474722835dac5e70bf126372f63
Pedro Lyrio <117763768+Pedro-Lyrio@users.noreply.github.com>
2024-06-23 19:06:32 -0300

    feat: cart carrinho

--- src/cart/entities/cart.entity.ts
+++ src/cart/entities/cart.entity.ts
@@ -12,8 +12,13 @@ import {
 export class CartEntity {
   @PrimaryGeneratedColumn('rowid')
   id: number;
+  
   @Column({ name: 'user_id', nullable: false })
   userId: number;
+
+  @Column({ name: 'active', nullable: false })
+  active: boolean;
+
   @CreateDateColumn({ name: 'created_at' })
   createdAt: Date;
 
