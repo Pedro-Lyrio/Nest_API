474fde3fd2551a2ed180ac983cd99fe0cc71a408
Pedro Lyrio <117763768+Pedro-Lyrio@users.noreply.github.com>
2024-06-23 21:40:32 -0300

    feat: testes unitatios classe carrinho

--- src/cart-product/__tests__/cart-product.service.spec.ts
+++ src/cart-product/__tests__/cart-product.service.spec.ts
@@ -1,16 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CartProductService } from '../cart-product.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { productMock } from 'src/product/__mocks__/product.mock';
-import { returnDeleteMock } from 'src/product/__mocks__/return-delete.mock';
-import { ProductService } from 'src/product/product.service';
+import { productMock } from '../../product/__mocks__/product.mock';
+import { returnDeleteMock } from '../../product/__mocks__/return-delete.mock';
+import { ProductService } from '../../product/product.service';
 import { Repository } from 'typeorm';
 import { CartProductEntity } from '../entities/cart-product.entity';
-import { cartMock } from 'src/cart/__mocks__/cart.mock';
+import { cartMock } from '../../cart/__mocks__/cart.mock';
 import { NotFoundException } from '@nestjs/common';
-import { insertCartMock } from 'src/cart/__mocks__/insert-cart.mock';
+import { insertCartMock } from '../../cart/__mocks__/insert-cart.mock';
 import { cartProductMock } from '../__mocks__/cart-product.mock';
-import { updateCartMock } from 'src/cart/__mocks__/update-cart.mock';
+import { updateCartMock } from '../../cart/__mocks__/update-cart.mock';
 
 describe('CartProductService', () => {
   let service: CartProductService;
