8fcde9cca5bad36888d18bdaf2751ee71b085b78
Pedro Lyrio <117763768+Pedro-Lyrio@users.noreply.github.com>
2024-06-25 06:36:41 -0300

    feat: função getOrders

--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -17,17 +17,16 @@ export class PaymentService {
       private readonly paymentRepository: Repository<PaymentEntity>,
     ) {}
   
-    async createPayment(
-      createOrderDTO: CreateOrderDTO,
-      products: ProductEntity[],
-      cart: CartEntity,
-    ): Promise<PaymentEntity> {
-      const finalPrice = cart.cartProduct
-        ?.map((cartProduct: CartProductEntity) => {
+    generateFinalPrice(cart: CartEntity, products: ProductEntity[]) {
+      if (!cart.cartProduct || cart.cartProduct.length === 0) {
+        return 0;
+      }
+  
+      return cart.cartProduct
+        .map((cartProduct: CartProductEntity) => {
           const product = products.find(
             (product) => product.id === cartProduct.productId,
           );
-          console.log('product', products);
           if (product) {
             return cartProduct.amount * product.price;
           }
@@ -35,29 +34,36 @@ export class PaymentService {
           return 0;
         })
         .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-
-      if (createOrderDTO.amountPayments) {
-        const paymentCreditCard = new PaymentCreditCardEntity(
-          PaymentType.Done,
-          0,
-          0,
-          0,
-          createOrderDTO,
-        );
-        return this.paymentRepository.save(paymentCreditCard);
-      } else if (createOrderDTO.codePix && createOrderDTO.datePayment) {
-        const paymentPix = new PaymentPixEntity(
-          PaymentType.Done,
-          finalPrice,
-          0,
-          finalPrice,
-          createOrderDTO,
-        );
-        return this.paymentRepository.save(paymentPix);
       }
-  
-      throw new BadRequestException(
-        'Amount Payments or code pix or date payment not found',
-      );
+
+      async createPayment(
+        createOrderDTO: CreateOrderDTO,
+        products: ProductEntity[],
+        cart: CartEntity,
+      ): Promise<PaymentEntity> {
+        const finalPrice = this.generateFinalPrice(cart, products);
+    
+        if (createOrderDTO.amountPayments) {
+          const paymentCreditCard = new PaymentCreditCardEntity(
+            PaymentType.Done,
+            finalPrice,
+            0,
+            finalPrice,
+            createOrderDTO,
+          );
+          return this.paymentRepository.save(paymentCreditCard);
+        } else if (createOrderDTO.codePix && createOrderDTO.datePayment) {
+          const paymentPix = new PaymentPixEntity(
+            PaymentType.Done,
+            finalPrice,
+            0,
+            finalPrice,
+            createOrderDTO,
+          );
+          return this.paymentRepository.save(paymentPix);
+        }
+        throw new BadRequestException(
+          'Amount Payments or code pix or date payment not found',
+        );
+      }
     }
-  }
